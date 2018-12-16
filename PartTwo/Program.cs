4cada1c88bb847771c523a779de3b40e8260aaed
Miha <Miha@DESKTOP-5T344OH>
2018-12-16 14:16:03 -0800

    More String methods added

--- PartTwo/Program.cs
+++ PartTwo/Program.cs
@@ -10,14 +10,48 @@ namespace PartTwo
             string upperCase = "SUMMER DAYS";
             // Call ToLower instance method, which returns a new copy.
             string lower = upperCase.ToLower();
-            Console.WriteLine("{0}", lower);
+            Console.WriteLine("1.toLower Method: {0}", lower);
            // Console.ReadLine();
 
             // 2. Trim
             string textWithSpaces = "  Coffee Time "; ;
             string textWithoutSpaces = textWithSpaces.Trim();
-            Console.WriteLine("{0}", textWithoutSpaces);
+            Console.WriteLine("2.TrimMethod: {0}", textWithoutSpaces);
             //Console.ReadLine();
+
+            // 3.Length
+            string a = "I like coffee";
+            Console.WriteLine("3.Length method: {0}", a.Length);
+
+            // 4.Substring
+            string fullName = "Miha Grigoras";
+            // Get first three characters.
+            string firstName = fullName.Substring(0, 4);
+            Console.WriteLine("4.Substring method: {0}", firstName);
+
+            // 5.Remove
+            string input = "MihaG";
+            string result = input.Remove(4);
+            Console.WriteLine("5.Remove method: {0}", result);
+
+            // 6. Concat
+            string lastName = "Grigoras";
+            string name = "Miha" + lastName;
+            Console.WriteLine("6.Concat method: {0}", name);
+
+            // 7.IsNullOrEmpty
+            string s = null;
+            if (string.IsNullOrEmpty(s) == true)
+            {
+                Console.WriteLine("7.IsNullOrEmpty method: Null or empty");
+            }
+            else
+            {
+                Console.WriteLine("7.IsNullOrEmpty method: Not null and not empty");
+            }
+            Console.ReadLine();
+
+
 
         }
     }
