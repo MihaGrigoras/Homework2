4da1e4223e05e456d655f3d448883d039a5530e0
Miha <Miha@DESKTOP-5T344OH>
2018-12-16 13:36:40 -0800

    Add the two projects of the solution

---
+++ PartTwo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PartTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. ToLower()
+            string upperCase = "SUMMER DAYS";
+            // Call ToLower instance method, which returns a new copy.
+            string lower = upperCase.ToLower();
+            Console.WriteLine("{0}", lower);
+           // Console.ReadLine();
+
+            // 2. Trim
+            string textWithSpaces = "  Coffee Time "; ;
+            string textWithoutSpaces = textWithSpaces.Trim();
+            Console.WriteLine("{0}", textWithoutSpaces);
+            //Console.ReadLine();
+
+        }
+    }
+}
