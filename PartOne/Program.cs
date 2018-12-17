6fdb4acbe956cfd93bb10bda43a90862d4e25d22
Miha <Miha@DESKTOP-5T344OH>
2018-12-17 13:00:25 -0800

    gender changes

--- PartOne/Program.cs
+++ PartOne/Program.cs
@@ -18,8 +18,7 @@ namespace PartOne
         static void Main(string[] args)
         {
 
-            //Convert the 3 integers and create a DateTime for the birthdate
-
+            // Convert the 3 integers and create a DateTime for the birthdate
             Console.Write("Please enter the year of birth: ");
             int year = int.Parse(Console.ReadLine());
 
@@ -33,35 +32,35 @@ namespace PartOne
 
             // Compute the age of the person, based on the birthdate 
             int age = (int)((DateTime.Now - birthDate).TotalDays/365.2425);
-            Console.WriteLine("Your age is");
-            Console.WriteLine(age);
+            Console.WriteLine("Your age is : {0}", age);
 
             //Colect the gender of the user
-            Console.Write("Please enter your gender (m/f): ");
+            Console.Write("Please enter your gender (M/F): ");
 
-            string genderTyped = Console.ReadLine().ToLower().Trim();
+            string letterTyped = Console.ReadLine().ToUpper().Trim();
+            Console.WriteLine("You typed : {0}", letterTyped);
 
-            Gender gender = Gender.Female;
-
-
-            switch (genderTyped)
+            //Gender gender = Gender.Female;
+            Gender? gender = null;
+   
+            switch (letterTyped)
             {
-                case "f":
-                    gender = Gender.Female;
-                    Console.WriteLine("Gender : Female");
-                    Console.ReadLine();
-                    break;
-                case "m":
+                case "M":
                     gender = Gender.Male;
-                    Console.WriteLine("Gender : Male");
+                    Console.WriteLine("Your gender : {0}", gender);
                     Console.ReadLine();
                     break;
-
-
+                case "F":
+                    gender = Gender.Female;
+                    Console.WriteLine("Your gender : {0}", gender);
+                    Console.ReadLine();
+                    break;
+                default:
+                    Console.WriteLine("The typed value is incorrect");
+                    Console.ReadLine();
+                    break;
 
             }
-
-
 
         }
     }
